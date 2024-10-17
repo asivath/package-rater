6d960ce989fc87f69263239bb9b8e395d2ca743e
Ellis Selznick <eselznic@purdue.edu>
2024-10-17 17:20:26 -0400

    changed playwright testing

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -14,19 +14,19 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './src/__tests__',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false, // Set to false to avoid fully parallel execution
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1, // Ensure only 1 worker is used for sequential execution
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'html' : 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
