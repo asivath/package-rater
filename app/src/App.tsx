f9f2605d57a59927b24e14bd154c4dd1187792f9
asivath <asivatha@purdue.edu>
2024-10-19 00:58:31 -0400

    added server and deploy workflows

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,5 @@
 import { SearchBar, NavBar } from "./components";
 import { Box } from "@mui/material";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 function App() {
   return (
